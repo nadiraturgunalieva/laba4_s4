f3666143f7a72d426c9ec44a7f4fead58f483bd5
Надира Тургуналиева <1132226224@pfur.ru>
2024-05-23 05:37:01 +0300

    опр шаблона

--- laba4_s4.cpp
+++ laba4_s4.cpp
@@ -1,7 +1,31 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+
+template <typename T>
+int findSecondMaxIndex(T* arr, int size) {
+    if (size < 2) {
+        std::cerr << "Array size must be at least 2" << std::endl;
+        return -1;
+    }
+
+    int maxIndex = 0;
+    int secondMaxIndex = -1;
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[maxIndex]) {
+            secondMaxIndex = maxIndex;
+            maxIndex = i;
+        }
+        else if (secondMaxIndex == -1 || arr[i] > arr[secondMaxIndex]) {
+            if (arr[i] != arr[maxIndex]) {
+                secondMaxIndex = i;
+            }
+        }
+    }
+
+    return secondMaxIndex;
+} 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }

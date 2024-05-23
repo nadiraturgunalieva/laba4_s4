aa9b2a4e0de3ad4cecaa0b799d87cb72b07465e7
Надира Тургуналиева <1132226224@pfur.ru>
2024-05-23 05:56:39 +0300

    main+

--- laba4_s4.cpp
+++ laba4_s4.cpp
@@ -10,7 +10,7 @@ int findSecondMaxIndex(T* arr, int size) {
     int maxIndex = 0;
     int secondMaxIndex = -1;
 
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; ++i) {
         if (arr[i] > arr[maxIndex]) {
             secondMaxIndex = maxIndex;
             maxIndex = i;
@@ -27,5 +27,16 @@ int findSecondMaxIndex(T* arr, int size) {
 
 int main()
 {
-    
+    float arrFloat[] = { 1.5, 7.6, 2.7, 6.8, 5.9 };
+    double arrDouble[] = { 2.5, 4.6, 3.7, 5.8 };
+    int arrInt[] = { 1, 3, 8, 4, 2 };
+    char arrChar[] = { 'i', 'l', 'y', 's', 'm' };
+
+    std::cout << "Index of second max in float array: " << findSecondMaxIndex(arrFloat, 5) << std::endl;
+    std::cout << "Index of second max in double array: " << findSecondMaxIndex(arrDouble, 4) << std::endl;
+    std::cout << "Index of second max in int array: " << findSecondMaxIndex(arrInt, 5) << std::endl;
+    std::cout << "Index of second max in char array: " << findSecondMaxIndex(arrChar, 5) << std::endl;
+
+    return 0;
+
 }

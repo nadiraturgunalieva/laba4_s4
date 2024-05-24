08eeaf16085dab0ea76f59c0690d928243b24228
Надира Тургуналиева <1132226224@pfur.ru>
2024-05-24 08:36:07 +0300

    Шаблон класса Vector

--- laba4_z3/laba4_z3.cpp
+++ laba4_z3/laba4_z3.cpp
@@ -1,7 +1,73 @@
 ﻿#include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+template <typename T>
+class Vector {
+private:
+    std::vector<T> vec;
+
+public:
+    Vector() {}
+
+    explicit Vector(int size) : vec(size) {}
+
+    Vector(const std::vector<T>& v) : vec(v) {}
+
+    T& operator[](int index) {
+        return vec[index];
+    }
+
+    const T& operator[](int index) const {
+        return vec[index];
+    }
+
+    Vector<T> operator*(T scalar) {
+        Vector<T> result(vec.size());
+        for (int i = 0; i < vec.size(); ++i) {
+            result[i] = vec[i] * scalar;
+        }
+        return result;
+    }
+
+    T operator*(const Vector<T>& other) {
+        T result = 0;
+        for (int i = 0; i < vec.size(); ++i) {
+            result += vec[i] * other[i];
+        }
+        return result;
+    }
+
+    int size() const {
+        return vec.size();
+    }
+
+    void printVector() {
+        for (int i = 0; i < vec.size(); ++i) {
+            std::cout << vec[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    Vector<int> v1({ 1, 2, 3 });
+    Vector<int> v2({ 3, 2, 4 });
+
+    std::cout << "Vector 1: ";
+    v1.printVector();
+
+    std::cout << "Vector 2: ";
+    v2.printVector();
+
+    Vector<int> result = v1 * 2;
+    std::cout << "Scalar multiplication: ";
+    for (int i = 0; i < result.size(); ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int dotProduct = v1 * v2;
+    std::cout << "Dot product: " << dotProduct << std::endl;
+
+    return 0;
 }
-
